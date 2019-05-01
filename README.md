d019d716de69959ac07d89402881ea9433c4c35f
Praveen kumar Jogi <praveenkumarjogi@Praveens-MacBook-Air.local>
2019-04-30 20:54:14 -0700

    update README and chef_api.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # chef
-Automate chef API tasks
+Script used to check the Hosted chef issues - https://status.chef.io/
+
+## Run script manually or use a cron job to monitor the hosted chef services
+
+```bash
+python chef_api.py
+```
--- chef_api.py
+++ chef_api.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
 """
-Script is used to check the chef service status page
+Script is used to check the chef status page
 """
 import urllib,json
 
 url = "http://status.chef.io/?format=json"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
-if data['components'][2]["status"] == "operational":
-    print(0)
-else:
-    print(1)
+if data['components'][2]["status"] != "operational":
+    print("Hosted Chef issues")
